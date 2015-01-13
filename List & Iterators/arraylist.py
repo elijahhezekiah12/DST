94e32635a9fa9d9d067b0e93add95952e9fe191f
Tanzim Hoque <tanzimhoque@gmail.com>
2015-01-13 15:56:34 +0000

    updated to folders

---
+++ List & Iterators/arraylist.py
@@ -0,0 +1,22 @@
+class ArrayList():
+	def __init__(self):
+		self.size = 0
+		self.s = []
+		
+	def size(self):
+		return self.size
+	
+	def isEmpty(self):
+		return size == 0
+	
+	def get(self, i):
+		return s[i]
+	
+	def replace(self, i, e):
+	
+	
+	def add(self, i, e):
+		self.s[i] = e
+	
+	def remove(self, i):
+		self.
