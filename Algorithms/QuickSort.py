7ab707b8ee7edb2c3593d36d04889be1c28f0acd
Tanzim Hoque <tanzimhoque@gmail.com>
2015-02-08 18:31:56 +0000

    added quicksort.py

---
+++ Algorithms/QuickSort.py
@@ -0,0 +1,31 @@
+import math
+
+def quicksort(list):
+	if len(list) > 1:
+		l, e, g = partition(list)	
+		return quicksort(l) + e + quicksort(g)	
+	else:
+		return list
+
+def partition(list):
+		pivot = list[len(list) - 1]
+		l = []
+		e = []
+		g = []
+		
+		for i in range(len(list)):
+			if list[i] > pivot:
+				g.append(list[i])
+			elif list[i] < pivot:
+				l.append(list[i])
+			else:
+				e.append(list[i])
+				
+		return l, e, g
+		
+
+
+
+if __name__ == "__main__":
+	list = [1,5,7,8,3,6,8,0,9]
+	print quicksort(list)
